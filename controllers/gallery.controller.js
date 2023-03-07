87b1e798e8a25768bd48173f49efe46edd74ef50
Aljith KJ <kwdeathlok@gmail.com>
2023-03-07 12:01:12 +0530

    some changes

--- controllers/gallery.controller.js
+++ controllers/gallery.controller.js
@@ -5,52 +5,38 @@ const Gallery = require('../models/gallery.model');
 
 const uploadImage = async (req, res) => {
     try {
-        const image = req.file.path;
-        if (!image) {
-            return res.status(400).json({ 
-                status: false,
-                message : 'No file was uploaded.' 
+        const { public_id, version, signature, image } = req.body;
+
+        const expectedSignature = cloudinary.utils.api_sign_request({ 
+            public_id: public_id, version: version 
+        }, process.env.CLOUDINARY_API_SECRET);
+
+        if (expectedSignature === signature) {
+            const thumbnail1Url = cloudinary.url(public_id, { width: 500, height: 500, crop: 'limit' });
+            const thumbnail2Url = cloudinary.url(public_id, { width: 250, height: 250, crop: 'limit' });
+            const thumbnail3Url = cloudinary.url(public_id, { width: 100, height: 100, crop: 'limit' });
+
+            const gallery = await Gallery.create({
+                img: image,
+                thumbnail_1: thumbnail1Url,
+                thumbnail_2: thumbnail2Url,
+                thumbnail_3: thumbnail3Url,
+                author_id: req.user._id
+            })
+
+             return res.status(200).json({ 
+                status: true,
+                message : 'Image uploaded successfully.',
+                gallery
             });
+        }else{
+            if(!result){
+                return res.status(400).json({ 
+                    status: false,
+                    message : 'No file was uploaded.' 
+                });
+            }
         }
-        const result = await cloudinary.uploader.upload(image, {
-            folder: 'uploads',
-            transformation: [
-                { width: 200, height: 200, crop: 'fill', gravity: 'face' },
-                { width: 400, height: 400, crop: 'fill', gravity: 'face' },
-                { width: 600, height: 600, crop: 'fill', gravity: 'face' }
-            ]
-        });
-
-        const imageUrl = result.secure_url;
-        const thumbnail1Url = cloudinary.url(result.public_id, { width: 500, height: 500, crop: 'limit' });
-        const thumbnail2Url = cloudinary.url(result.public_id, { width: 250, height: 250, crop: 'limit' });
-        const thumbnail3Url = cloudinary.url(result.public_id, { width: 100, height: 100, crop: 'limit' });
-        
-        if(!result){
-            return res.status(400).json({ 
-                status: false,
-                message : 'No file was uploaded.' 
-            });
-        }
-
-        // Deleting image from upload folder
-        fs.unlink(image, (err) => {
-            if(err) console.log(err);
-        })
-
-        const gallery = await Gallery.create({
-            img: imageUrl,
-            thumbnail_1: thumbnail1Url,
-            thumbnail_2: thumbnail2Url,
-            thumbnail_3: thumbnail3Url,
-            author_id: req.user._id
-        })
-
-        return res.status(200).json({ 
-            status: true,
-            message : 'Image uploaded successfully.',
-            gallery
-        });
 
     } catch (err) {
         console.error(err.message);
@@ -114,9 +100,34 @@ const deleteImage = async (req, res) => {
     }
 }
 
+const getSignature = async (req, res) => {
+    try {
+        const timestamp = Math.round(new Date().getTime() / 1000);
+        const signature = cloudinary.utils.api_sign_request(
+            {
+                timestamp: timestamp
+            },
+            process.env.CLOUDINARY_API_SECRET
+        )
+
+        res.status(200).json({
+            timestamp,
+            signature
+        })
+        
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send({
+            status: false,
+            message: 'Server error'
+        }); 
+    }
+}
+
 module.exports = {
     uploadImage,
     getAllImages,
     getThumbnails,
-    deleteImage
+    deleteImage,
+    getSignature
 }
--- routes/gallery.js
+++ routes/gallery.js
@@ -1,15 +1,14 @@
 const { Router } = require('express');
-const multer = require('multer');
-const { uploadImage, getAllImages, getThumbnails, deleteImage } = require('../controllers/gallery.controller'); 
+const { uploadImage, getAllImages, getThumbnails, deleteImage, getSignature } = require('../controllers/gallery.controller'); 
 const authorize = require('../middlewares/authorization');
 
-const upload = multer({ dest: './uploads/' });
 const router = Router();
 
 router.get('/', authorize, getAllImages);
 router.delete('/delete-image/:id', authorize, deleteImage);
 router.get('/thumbnails/:id', authorize, getThumbnails);
-router.post('/upload', upload.single('image'), authorize, uploadImage );
+router.post('/upload', authorize, uploadImage );
+router.get('/get-signature', authorize, getSignature);
 
 
  
